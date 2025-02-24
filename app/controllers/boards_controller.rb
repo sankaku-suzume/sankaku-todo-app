5ead2c00958f34aa38002bdceb368009c5f76c65
さんかく <sankaku.suzume@gmail.com>
2025-02-25 05:52:06 +0900

    fakerによるダミーデータ作成、indexアクション、createアクション

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,7 +1,17 @@
 class BoardsController < ApplicationController
 
   def index
-    # @boards = Boards.all
+    @boards = Board.all
+  end
+
+  def create
+    @board = current_user.boards.build(boards_params)
+    @board.save
+  end
+
+  private
+  def boards_params
+    params.require(:board).permit(:title, :content)
   end
 
 end
