6c5aad6b552f9ab45ce8951682b515a4851c2e60
さんかく <sankaku.suzume@gmail.com>
2025-03-23 17:51:26 +0900

    プロフィール情報の表示、delegateを使用

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,8 @@ class User < ApplicationRecord
   has_many :boards, dependent: :destroy
   has_one :profile, dependent: :destroy
 
+  delegate :birthday, :age, :gender, to: :profile, allow_nil: true
+
   def has_written?(board)
     boards.exists?(id: board.id)
   end
