5aa2d0fec92eaa8d55aacd815aeb556bcf24887b
さんかく <sankaku.suzume@gmail.com>
2025-03-30 14:55:27 +0900

    showアクション

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,10 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
+  def show
+    @task = Task.find(params[:id])
+  end
+
   def new
     board = Board.find(params[:board_id])
     @task = board.tasks.build
