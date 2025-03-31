4c5159336934e9975bfe3fc661c78e6cd2e49b23
さんかく <sankaku.suzume@gmail.com>
2025-03-31 21:34:29 +0900

    edit、updateアクション

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -21,6 +21,20 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+    @task = current_user.tasks.find(params[:id])
+  end
+
+  def update
+    @task = current_user.tasks.find(params[:id])
+    if @task.update(task_params)
+      redirect_to board_task_path(@task.board_id, @task.id), notice: 'Successfully updated.'
+    else
+      flash.now[:error] = 'Failed to update.'
+      render :edit
+    end
+  end
+
   private
   def task_params
     params.require(:task).permit(
