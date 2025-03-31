7688724d015060d73bfc8da8eab449fe26ac9f2e
さんかく <sankaku.suzume@gmail.com>
2025-04-01 06:00:32 +0900

    newアクション、createアクション

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,27 @@
+class CommentsController < ApplicationController
+
+  def new
+    @task = Task.find(params[:task_id])
+    @comment = @task.comments.build
+  end
+
+  def create
+    @task = Task.find(params[:task_id])
+    @comment = @task.comments.build(comment_params)
+    if @comment.save
+      redirect_to board_task_path(@task.board_id, @task.id), notice: 'Successfully saved.'
+    else
+      flash.now[:error] = 'Failed to save.'
+      render :new
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content).
+    merge(
+      user_id: current_user.id
+    )
+  end
+
+end
